535f523496d98ceac6ef373a4dcea0d515671d57
LuvianJACL <115852426+LuvianJACL@users.noreply.github.com>
2023-05-04 16:36:25 -0600

    Introducción de datos

---
+++ Introduccion_Datos.m
@@ -0,0 +1,9 @@
+clc
+clear all
+close all
+
+numero_A = input("Dame un número: ");
+numero_B = input("Dame otro número: ");
+
+operacion = numero_A + numero_B;
+disp(["El resultado de la operación es: " + num2str(operacion)])
