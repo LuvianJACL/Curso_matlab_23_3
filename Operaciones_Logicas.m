14671475a88ee96899c7d5fa22e231a47f6095c4
LuvianJACL <115852426+LuvianJACL@users.noreply.github.com>
2023-05-04 17:24:32 -0600

    Operaciones_Lógicas

---
+++ Operaciones_Logicas.m
@@ -0,0 +1,23 @@
+clc
+clear all
+close all
+
+%Son las operaciones cuyo resultado es un valor verdadero o un valor falso
+%AND Es una operación cuyo resultado es 1, si ambos valoees que se combinan
+%son 1, de lo contrario son 0
+
+%OR es una operación donde el resultado es 1, si alguno de los 2 valores es
+%1
+
+%NOT Es invertir el valor de verdad
+
+%AND &
+%OR |
+%NOT ~
+%Verdadero 1, true
+%Falso 0, false
+
+operacion_1 = false & false
+operacion_2 = true | false
+operacion_3 = ~1
+
